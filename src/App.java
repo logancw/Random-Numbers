0ce012b141ea1c2bda80e53ea2dc9ec202871591
clokyy <clokyymc@gmail.com>
2023-11-30 20:25:27 -0600

    + Project

---
+++ src/App.java
@@ -0,0 +1,21 @@
+class App {
+    public static void main(String[] args) {
+      for(int i = 0; i <= 10; i++){
+        double a = Math.random() * 10;
+        double b = Math.random() * 10;
+  
+        System.out.println(compare((int) a, (int) b));
+      }
+    }
+  
+  
+    public static String compare(int a, int b){
+      if(a > b){
+        return a + "  >  " + b;
+      }else if(b > a){
+        return b + "  >  " + a;
+      }else{
+        return a + "  =  " + b;
+      }
+    }
+  }
